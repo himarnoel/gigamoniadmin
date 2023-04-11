bba2cae75b85a6ab4707cd3b7ecc3854b718e48b
himarnoel <olaimarnoel@gmail.com>
2023-04-11 21:47:47 +0100

    gone

--- src/Paths/Home.jsx
+++ src/Paths/Home.jsx
@@ -20,15 +20,44 @@ const Home = (props) => {
           <p className="mt-5 text-4xl font-semibold">0</p>
         </div>
       </div>
-      <div className="flex mt-10 justify-around w-[30rem]">
-        <p className="text-sm font-semibold  cursor-pointer">
+      <div className="flex mt-10 justify-around w-[37rem] items-center">
+        <p className="text-xl font-semibold  cursor-pointer">
           Transaction activities
         </p>
-        <p className="ss cursor-pointer">This Week</p>
-        <p className="ss cursor-pointer">This Month</p>
-        <p className="sd cursor-pointer">This Year</p>
+        <p className="text-sm cursor-pointer">This Week</p>
+        <p className="text-sm cursor-pointer text-[#009186] font-bold">
+          This Month
+        </p>
+        <p className="text-sm cursor-pointer text-[#009186] font-bold">
+          This Year
+        </p>
       </div>
-      <div className="w-full bg-[#DAF2F1] h-[25rem] mt-10 rounded-2xl "></div>
+      <div className="w-full bg-[#DAF2F1] min-h-[30rem] mt-10 rounded-2xl px-5 py-8">
+        <div className="flex flex-col justify-between gap-y-4">
+          <p className="">70</p>
+          <p className="">65</p>
+          <p className="">60</p>
+          <p className="">55</p>
+          <p className="">50</p>
+          <p className="">45</p>
+          <p className="">40</p>
+          <p className="">35</p>
+          <p className="">30</p>
+          <p className="">25</p>
+          <p className="">20</p>
+          <p className="">15</p>
+          <p className="">10</p>
+          <p className="">5</p>
+        </div>
+        <div className="flex w-full justify-between px-8 ">
+          <p className="ss lowercase">MON</p>
+          <p className="ss lowercase">TUE</p>
+          <p className="ss lowercase">WES</p>
+          <p className="ss lowercase">THUR</p>
+          <p className="ss lowercase">FRI</p>
+          <p className="ss lowercase">SAT</p>
+        </div>
+      </div>
       <div className="flex mt-10 w-full h-[1rem]"></div>
     </div>
   );
