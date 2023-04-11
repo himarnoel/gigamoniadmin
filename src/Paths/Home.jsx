2e46611ab17e69bc62592ef81d133857fe90affa
himarnoel <olaimarnoel@gmail.com>
2023-04-11 19:51:50 +0100

    worked on home

--- src/Paths/Home.jsx
+++ src/Paths/Home.jsx
@@ -6,9 +6,19 @@ const Home = (props) => {
   return (
     <div className={`font-poppins bg-[#F8F8FF]  h-screen pt-20 px-6`}>
       <p className="text-2xl font-semibold text-[#175873]">Activity Overview</p>
-      <div className="rounded-lg bg-[#87ACA3] flex flex-col text-xl   pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
-        <p className="d">Number of Users</p>
-        <p className="mt-5 text-4xl font-semibold">0</p>
+      <div className="flex w-full gap-x-4 mt-4">
+        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+          <p className="text-base">Number of Users</p>
+          <p className="mt-5 text-4xl font-semibold">0</p>
+        </div>
+        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+          <p className="text-base">Fund Transfer Request</p>
+          <p className="mt-5 text-4xl font-semibold">0</p>
+        </div>
+        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+          <p className="text-base">Pending Transactions</p>
+          <p className="mt-5 text-4xl font-semibold">0</p>
+        </div>
       </div>
     </div>
   );
