f6255923fb94575417f1fed5e1bc60c72cb3d202
himarnoel <olaimarnoel@gmail.com>
2023-04-11 19:45:07 +0100

    working on home

--- src/Paths/Home.jsx
+++ src/Paths/Home.jsx
@@ -4,13 +4,11 @@ import SideNav from "../Components/SideNav";
 
 const Home = (props) => {
   return (
-    <div
-      className={`font-poppins bg-blue-400 flex justify-center items-center h-screen `}
-    >
-      {/* <Nav />
-      <SideNav /> */}
-      {/* <div className="sd"> hOMESDFASDNFASKDNFALSDNFLKN</div> */}
-     Home
+    <div className={`font-poppins bg-blue-100  h-screen pt-20 px-6`}>
+      <p className="text-2xl font-semibold text-[#175873]">Activity Overview</p>
+      <div className="rounded-lg bg-[#87ACA3] flex flex-col justify-center">
+        Number of Users
+      </div>
     </div>
   );
 };
