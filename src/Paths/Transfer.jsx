b6605b544532d26b83008d44c4749344a690df72
himarnoel <olaimarnoel@gmail.com>
2023-04-12 00:12:50 +0100

    transfer

--- src/Paths/Transfer.jsx
+++ src/Paths/Transfer.jsx
@@ -6,11 +6,13 @@ const Transfer = () => {
       <p className="text-2xl font-semibold text-[#175873]">
         Fund Transfer Requests
       </p>
-      <input
-        type="text rounded"
-        placeholder="Search transcations"
-        className="appearance-none focus:ring-0 focus: text-xs"
-      />
+      <div className="flex">
+        <input
+          type="text rounded"
+          placeholder="Search transcations"
+          className="appearance-none focus:ring-0 focus:outline-none py-3 px-2 w-[20rem] mt-6 rounded-lg border-2 border-[#009186] text-xs"
+        />
+      </div>
 
       <div className="flex mt-10 w-full h-[1rem]"></div>
     </div>
