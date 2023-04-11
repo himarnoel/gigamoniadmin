8de017a64ba998620bf74fe84febb4dd66967d52
himarnoel <olaimarnoel@gmail.com>
2023-04-11 19:48:25 +0100

    working on the zero

--- src/Paths/Home.jsx
+++ src/Paths/Home.jsx
@@ -4,10 +4,11 @@ import SideNav from "../Components/SideNav";
 
 const Home = (props) => {
   return (
-    <div className={`font-poppins bg-blue-100  h-screen pt-20 px-6`}>
+    <div className={`font-poppins bg-[#F8F8FF]  h-screen pt-20 px-6`}>
       <p className="text-2xl font-semibold text-[#175873]">Activity Overview</p>
-      <div className="rounded-lg bg-[#87ACA3] flex flex-col justify-center">
-        Number of Users
+      <div className="rounded-lg bg-[#87ACA3] flex flex-col text-xl   pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+        <p className="d">Number of Users</p>
+        <p className="mt-5 text-4xl font-semibold">0</p>
       </div>
     </div>
   );
