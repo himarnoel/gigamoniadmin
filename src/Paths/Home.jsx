52034ea9afc05a62d37b701f3ce7d64994ed1008
himarnoel <olaimarnoel@gmail.com>
2023-04-11 20:21:05 +0100

    working on transaction

--- src/Paths/Home.jsx
+++ src/Paths/Home.jsx
@@ -4,7 +4,7 @@ import SideNav from "../Components/SideNav";
 
 const Home = (props) => {
   return (
-    <div className={`font-poppins bg-[#F8F8FF]  h-screen pt-24 px-6`}>
+    <div className={`font-poppins bg-[#F8F8FF]  h-screen pt-24 px-6 `}>
       <p className="text-2xl font-semibold text-[#175873]">Activity Overview</p>
       <div className="flex w-full gap-x-4 mt-8">
         <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
@@ -20,13 +20,14 @@ const Home = (props) => {
           <p className="mt-5 text-4xl font-semibold">0</p>
         </div>
       </div>
-      <div className="flex">
-        <p className="ss">Transaction activities</p>
+      <div className="flex mt-10 justify-around w-[30rem]">
+        <p className="text-sm font-semibold ">Transaction activities</p>
         <p className="ss">This Week</p>
         <p className="ss">This Month</p>
         <p className="sd">This Year</p>
       </div>
-      <div className="w-full bg-[#DAF2F1] h-[25rem] mt-10 rounded-2xl"></div>
+      <div className="w-full bg-[#DAF2F1] h-[25rem] mt-10 rounded-2xl "></div>
+      <div className="flex mt-10 w-full h-[1rem]"></div>
     </div>
   );
 };
