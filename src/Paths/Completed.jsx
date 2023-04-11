17c606eb6e20817079cdd49b9a9c1e1d63f23824
himarnoel <olaimarnoel@gmail.com>
2023-04-11 02:07:11 +0100

    completed

--- src/Paths/Completed.jsx
+++ src/Paths/Completed.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Completed = () => {
-  return <div>Completed</div>;
+  return (
+    <div>
+      <div
+        className={`font-poppins bg-blue-400 flex justify-center items-center h-screen `}
+      >
+        {/* <Nav />
+  <SideNav /> */}
+        {/* <div className="sd"> hOMESDFASDNFASKDNFALSDNFLKN</div> */}
+        Completed
+      </div>
+    </div>
+  );
 };
 
 export default Completed;
