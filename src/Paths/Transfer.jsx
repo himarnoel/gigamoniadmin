2049839128149df631ae41ddfe1cbe47aeddcc7b
himarnoel <olaimarnoel@gmail.com>
2023-04-11 21:56:30 +0100

    transfer

--- src/Paths/Transfer.jsx
+++ src/Paths/Transfer.jsx
@@ -6,6 +6,11 @@ const Transfer = () => {
       <p className="text-2xl font-semibold text-[#175873]">
         Fund Transfer Requests
       </p>
+      <input
+        type="text rounded"
+        placeholder="Search transcations"
+        className="appearance-none focus:ring-0 focus: text-xs"
+      />
 
       <div className="flex mt-10 w-full h-[1rem]"></div>
     </div>
