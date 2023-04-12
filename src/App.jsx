65d39a64d74ed28e3c65c83285891ef19e2d79ca
himarnoel <olaimarnoel@gmail.com>
2023-04-12 17:46:12 +0100

    remaned folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Paths/Home";
+import Home from "./DashboardPages/Home";
 import NavBar from "./Components/Nav";
 import NavPage from "./Pages/NavPage";
 import MainPage from "./Pages/MainPage";
--- src/Pages/NavPage.jsx
+++ src/Pages/NavPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../Paths/Home";
-import Transfer from "../Paths/Transfer";
-import Pending from "../Paths/Pending";
-import Rejected from "../Paths/Rejected";
-import Completed from "./../Paths/Completed";
-import TransferRequestDetails from "./../Paths/TransferRequestDetails";
+import Home from "../DashboardPages/Home";
+import Transfer from "../DashboardPages/Transfer";
+import Pending from "../DashboardPages/Pending";
+import Rejected from "../DashboardPages/Rejected";
+import Completed from "../DashboardPages/Completed";
+import TransferRequestDetails from "../DashboardPages/TransferRequestDetails";
 
 const NavPage = () => {
   return (
@@ -16,7 +16,7 @@ const NavPage = () => {
         <Route path="/pending" element={<Pending />} />
         <Route path="/rejected" element={<Rejected />} />
         <Route path="/completed" element={<Completed />} />
-        
+
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
