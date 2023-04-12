ab40901f315cd3db58ddd5740c97690893db28ba
himarnoel <olaimarnoel@gmail.com>
2023-04-12 22:40:28 +0100

    path

--- src/Pages/Paths.jsx
+++ src/Pages/Paths.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../DashboardPages/Home";
-import Transfer from "../DashboardPages/Transfer";
-import Pending from "../DashboardPages/Pending";
-import Rejected from "../DashboardPages/Rejected";
-import Completed from "../DashboardPages/Completed";
-import TransferRequestDetails from "../OutsidePages/TransferRequestDetails";
-import MainPage from "./MainPage";
+
 
 const NavPage = () => {
   return (
