4b62e2bbb904b09c5c16b7379315fd394f593a78
himarnoel <olaimarnoel@gmail.com>
2023-04-13 18:12:31 +0100

    transfer

--- src/OutsidePages/TransferRequestDetails.jsx
+++ src/OutsidePages/TransferRequestDetails.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import Nav from "../Components/Nav";
+import { useFormik } from "formik";
+import { Validaterequest } from "../Service/Validate";
 
 const TransferRequestDetails = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      address: "",
+      country:"",
+      bvn: "",
+    },
+    validationSchema: Validaterequest,
+    onSubmit: (values) => {}
+  });
   return (
     <div className={`font-poppins   `}>
       <Nav />
@@ -19,16 +33,48 @@ const TransferRequestDetails = () => {
 
         <div className="flex  gap-x-[3.9rem] text-sm mt-10">
           <p className="">Mode of payment: Card Payment</p>
-          <p className="font-medium text-[#000000]">Local Currency:₦</p>
+          <p className="font-medium text-[#000000]">Local Currency: ₦</p>
         </div>
         <div className="text-[#262626] text-sm  justify-between grid grid-cols-2 mt-10">
           <div className="1 ">
             {" "}
             <p className="">Sender’s Details</p>
+            <div className="relative z-0 mt-0">
+              <input
+                type="text"
+                id="name"
+                className={
+                  formik.errors.name && formik.touched.name
+                    ? "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px]  border-red-500 appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
+                    : "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px] border-[#262626] appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
+                }
+                placeholder=" "
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                onBlur={formik.handleBlur}
+              />
+              <label
+                for="name"
+                className={
+                  formik.errors.name && formik.touched.name
+                    ? "absolute text-xs mxl:text-sm font-poppins text-red-500  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100  peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    : "absolute text-xs mxl:text-sm font-poppins text-[#262626]  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                }
+              >
+                Name
+              </label>
+              {formik.errors.name && formik.touched.name ? (
+                <p className="text-red-500 text-xs font-poppins">
+                  {formik.errors.name}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
           <div className="2 ">
             {" "}
-            <p className="">Sender’s Details</p>
+            <p className="">Receiver's Details</p>
           </div>
         </div>
       </div>
--- src/Service/Validate.js
+++ src/Service/Validate.js
@@ -1,31 +1,21 @@
 import * as yup from "yup";
 
 export const baseurl = "https://gigamoni-backend.onrender.com/api/v1";
-
-export const signupValidate = yup.object().shape({
-    name: yup.string().required("Please fill up this field"),
-    email: yup
-      .string()
-      .email("please enter a valid email")
-      .required("Please fill up this field"),
-    phonenumber: yup
-      .string()
-      .required("Please fill up this field")
-      .matches(phoneRegExp, "Phone number is not valid")
-      .max(15, "too long"),
-    country: yup.string().required("Please fill up this field"),
-    password: yup
-      .string()
-      .min(8, "password must containat least 8 characters ")
-      .matches(
-        passwordRegExp,
-        "characters with at least one of each: uppercase, lowercase, number and special"
-      )
-      .required("Please fill up this field"),
-    passwordConfirmation: yup
-      .string()
-      .oneOf([yup.ref("password"), null], "Passwords must match")
-      .required("Please fill up this field"),
-  });
+const phoneRegExp =
+  /^((\+[0-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+export const Validaterequest = yup.object().shape({
+  name: yup.string().required("Please fill up this field"),
+  email: yup
+    .string()
+    .email("please enter a valid email")
+    .required("Please fill up this field"),
+  phonenumber: yup
+    .string()
+    .required("Please fill up this field")
+    .matches(phoneRegExp, "Phone number is not valid")
+    .max(15, "too long"),
+  country: yup.string().required("Please fill up this field"),
+  address: yup.string().required("Please fill up this field"),
+  bvn: yup.string().required("Please fill up this field"),
   
-  
+});
