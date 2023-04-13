4c02f8a764836a167a4aa49143b12e325b30749e
himarnoel <olaimarnoel@gmail.com>
2023-04-13 11:08:32 +0100

    transfer

--- src/OutsidePages/TransferRequestDetails.jsx
+++ src/OutsidePages/TransferRequestDetails.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Nav from "../Components/Nav";
 
 const TransferRequestDetails = () => {
-  return <div>
-
-
-  </div>;
+  return (
+    <div className={`font-poppins bg-[#F8F8FF]  h-screen  `}>
+      <Nav />
+      <div className="pt-24 px-12"></div>
+    </div>
+  );
 };
 
 export default TransferRequestDetails;
