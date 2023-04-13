9dc384a70e51187ceb757dbd15250c81f1d52e85
himarnoel <olaimarnoel@gmail.com>
2023-04-13 18:21:02 +0100

    country

--- src/OutsidePages/TransferRequestDetails.jsx
+++ src/OutsidePages/TransferRequestDetails.jsx
@@ -157,7 +157,7 @@ const TransferRequestDetails = () => {
                     : "absolute text-xs mxl:text-sm font-poppins text-[#262626]  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 }
               >
-                Phone Number
+               Country
               </label>
               {formik.errors.country && formik.touched.country ? (
                 <p className="text-red-500 text-xs font-poppins">
@@ -167,6 +167,8 @@ const TransferRequestDetails = () => {
                 ""
               )}
             </div>
+            
+            
           </div>
           <div className="2 ">
             {" "}
