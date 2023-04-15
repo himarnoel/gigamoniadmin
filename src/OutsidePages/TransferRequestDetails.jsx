2c3166946893c2bd93fa2a3bb2546f915802f871
himarnoel <olaimarnoel@gmail.com>
2023-04-15 08:33:11 +0100

    transfer

--- src/OutsidePages/TransferRequestDetails.jsx
+++ src/OutsidePages/TransferRequestDetails.jsx
@@ -116,38 +116,7 @@ const TransferRequestDetails = () => {
                 ""
               )}
             </div>
-            <div className="relative z-0 mt-0">
-              <input
-                type="text"
-                id="phonenumber"
-                className={
-                  formik.errors.phoneNumber && formik.touched.phoneNumber
-                    ? "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px]  border-red-500 appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
-                    : "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px] border-[#262626] appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
-                }
-                placeholder=" "
-                onChange={formik.handleChange}
-                value={formik.values.phoneNumber}
-                onBlur={formik.handleBlur}
-              />
-              <label
-                for="phoneNumber"
-                className={
-                  formik.errors.phoneNumber && formik.touched.phoneNumber
-                    ? "absolute text-xs mxl:text-sm font-poppins text-red-500  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100  peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                    : "absolute text-xs mxl:text-sm font-poppins text-[#262626]  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                }
-              >
-                Address
-              </label>
-              {formik.errors.phoneNumber && formik.touched.phoneNumber ? (
-                <p className="text-red-500 text-xs font-poppins">
-                  {formik.errors.phoneNumber}
-                </p>
-              ) : (
-                ""
-              )}
-            </div>
+           
             <div className="relative z-0 mt-0">
               <input
                 type="text"
