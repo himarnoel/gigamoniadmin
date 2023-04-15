592297d4fbea331f66820debd3f42812395948ac
himarnoel <olaimarnoel@gmail.com>
2023-04-15 08:43:09 +0100

    edited a little

--- src/OutsidePages/TransferRequestDetails.jsx
+++ src/OutsidePages/TransferRequestDetails.jsx
@@ -418,7 +418,7 @@ const TransferRequestDetails = () => {
                 onBlur={formik.handleBlur}
               />
               <label
-                for="name"
+                for="receiverbankAddress"
                 className={
                   formik.errors.receiverbankAddress && formik.touched.receiverbankAddress
                     ? "absolute text-xs mxl:text-sm font-poppins text-red-500  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100  peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
