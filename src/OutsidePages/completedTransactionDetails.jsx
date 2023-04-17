3c18887eee900e9fbf9d1270f6cef22d4d2cbb7b
himarnoel <olaimarnoel@gmail.com>
2023-04-17 20:38:20 +0100

    completed

---
+++ src/OutsidePages/completedTransactionDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const completedTransactionDetails = () => {
+  return (
+    <div>completedTransactionDetails</div>
+  )
+}
+
+export default completedTransactionDetails
