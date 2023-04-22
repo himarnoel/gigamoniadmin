890f13b594e2946e936981d4bd941245da715880
himarnoel <olaimarnoel@gmail.com>
2023-04-22 20:30:34 +0100

    removed bug

--- src/DashboardPages/Home.jsx
+++ src/DashboardPages/Home.jsx
@@ -49,17 +49,17 @@ const Home = (props) => {
 
       <p className="text-2xl font-semibold text-[#175873]">Activity Overview</p>
       <div className="flex flex-col gap-y-4  md:flex-row w-full md:gap-x-4 mt-8">
-        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-full sm:w-[20rem] px-2 text-[#F8F8FF]">
           <p className="text-base">Number of Users</p>
           <p className="mt-5 text-4xl font-semibold">{data.noOfUsers}</p>
         </div>
-        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-full sm:w-[20rem] px-2 text-[#F8F8FF]">
           <p className="text-base">Fund Transfer Request</p>
           <p className="mt-5 text-4xl font-semibold">
             {data.fundTransferRequests}
           </p>
         </div>
-        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-[20rem] px-2 text-[#F8F8FF]">
+        <div className="rounded-lg bg-[#87ACA3] flex flex-col    pt-10 items-center h-[10rem] w-full sm:w-[20rem] px-2 text-[#F8F8FF]">
           <p className="text-base">Pending Transactions</p>
           <p className="mt-5 text-4xl font-semibold">
             {data.pendingTransactions}
